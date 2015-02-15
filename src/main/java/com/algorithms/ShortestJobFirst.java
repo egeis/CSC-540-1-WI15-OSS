5af7ed248163126d82ac9079b42dc2a66804fb46
egeis <rcoan@callutheran.edu>
2015-02-14 19:18:33 -0800

    Initial Push.

---
+++ src/main/java/com/algorithms/ShortestJobFirst.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.algorithms;
+
+import java.util.Map;
+import main.java.com.Job;
+
+/**
+ *
+ * @author Richard Coan
+ */
+public class ShortestJobFirst {
+    
+    public static void run(Map<Integer, Job> jobs)
+    {
+        
+        
+        
+    }
+    
+}
