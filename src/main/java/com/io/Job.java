abfe72597e2eaae119bf7182cf20c84c1ada29bc
egeis <rcoan@callutheran.edu>
2015-02-14 18:30:36 -0800

    Added Map for work done for simulated a simulated job.

--- src/main/java/com/io/Job.java
+++ src/main/java/com/io/Job.java
@@ -1,40 +1,55 @@
 package main.java.com.io;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 /**
  * Simulated Process Job.
  * @author Richard Coan
  */
 public class Job {
-    private int id;
-    private String name;
     private int waitTime;
+    private int ticks;
     private boolean completed;
+    
+    /** 
+     * work progress used for gantt chart, 
+     * 0 = Accepted at counter time.
+     * 1 or more represents the time frame ie. 1 = from time 0 to 1.
+     */
+    private Map<Long, Integer> work = new HashMap();
 
-    public Job(int id, String name, int waitTime)
+    public Job(int waitTime)
     {
-        this.id = id;
-        this.name = name;
         this.waitTime = waitTime;
         this.completed = false;
+        this.ticks = 0;
+        work.put(0L,0);             
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getWaitTime() {
-        return waitTime;
-    }
-    
+ 
     public boolean isCompleted() {
         return completed;
     }
+
+    public Map<Long, Integer> getWork() {
+        return work;
+    }
+
+    /**
+     * Increments Simulated Process Clock Work Progress.
+     * @param counter 
+     */
+    public void doTick(long counter)
+    {
+        if(++ticks == waitTime)
+            completed = true;
+        
+        work.put(counter,ticks);
+    }
         
     public String toString() {
-        return "Job "+this.id+": name="+this.name+" waitTime="+this.waitTime+" completed="+this.completed;
+        return "[Job: waitTime="+this.waitTime+" completed="+this.completed+" progress="+work.toString()+"]";
     }
 }
